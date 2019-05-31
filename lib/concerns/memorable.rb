d542416ee8d2596749fc99bc1b40f3771f418c13
Chris Kay <chriskay25@gmail.com>
2019-05-31 01:22:25 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,20 @@
 module Memorable
+  module ClassMethods
   
-  def reset_all
-    all.clear
-  end
+    def reset_all
+      all.clear
+    end
 
-  def count
-    all.count
-  end
+    def count
+      all.count
+    end
+  end 
+  
+  module InstanceMethods
+    
+    def initialize
+      
+    end 
+  end 
   
 end 
